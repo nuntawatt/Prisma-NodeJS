b1234febca4d91ff68794d7b4d3e825f8e607355
Nuntawat Saehuam <nanthawat.s@kkumail.com>
2025-11-26 23:23:05 +0700

    prisma database

---
+++ prisma/migrations/20251126153355/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "admin" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admin_email_key" ON "admin"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admin_username_key" ON "admin"("username");
